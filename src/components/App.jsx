42864b1cdb59fc0e0084d9bcda2c2fb8537faddb
Samy <200020040@iitdh.ac.in>
2021-10-11 21:49:06 +0530

    Updated Readme and Removed Hardcoded Notes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import Notes from "../Notes";
 import Add from './Add';
 
 function App() {
 
-    const [notes, setNotes] = React.useState(Notes);
+    const [notes, setNotes] = React.useState([]);
 
     return (
         <div>
